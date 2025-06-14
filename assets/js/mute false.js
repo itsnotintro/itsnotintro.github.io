8a449298fc28f80829f3cb5ef48c9a5eda5fc99e
itsnotintro <itsnotintro@gmail.com>
2025-06-15 01:47:13 +0900

    Initial upload

--- assets/js/mute false.js
+++ assets/js/mute false.js
@@ -1,6 +1,5 @@
 const audio = document.getElementById("typing");
 
-document.getElementById("unmuteBtn").addEventListener("click", function () {
+document.getElementById("play").addEventListener("click", function () {
   audio.muted = false;
-  audio.play(); // 재생이 안되고 있을 경우를 대비해 한 번 더 호출 (이미 재생 중이면 아무 일도 안 생깁니다)
-});
+  audio.play();
